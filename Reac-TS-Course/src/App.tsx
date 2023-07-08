24123943bfc0235c324dfa52ec538441a45a95e8
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2023-07-07 18:03:20 -0600

    added todo and todos

--- Reac-TS-Course/src/App.tsx
+++ Reac-TS-Course/src/App.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FirstPage from './Pages/MyFisrtPage'
 import InputField from './Components/Inputs/InputField'
+import todoModel from './Models/todoModel'
 import './App.css'
 
 
-let App: React.FC = () => {
+const App = (): JSX.Element => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<todoModel[]>([]);
+
+  const handleAdd = (e: React.FormEvent) =>{
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+    }
+  };
+
+
+  console.log(todo);
+  console.log(todos);
   return (
     <>
       <div className='App'>
         <span className="heading" style={{ color: "white" }}>Taskify!</span>
-        <InputField />
+        <InputField todo ={todo} setTodo = {setTodo} handleAdd ={handleAdd}/>
         <span style={{color: "white"}}>{}</span>
       </div>
 
---
+++ Reac-TS-Course/src/Models/todoModel.ts
@@ -0,0 +1,7 @@
+interface todo{
+    id: number;
+    todo: string;
+    isDone: boolean;
+}
+
+export default todo;
