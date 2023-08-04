c628645687f2761f200d1846f5d7fb47d1a0d40a
Armando Ibarra <70788529+Strako@users.noreply.github.com>
2023-08-04 16:26:16 -0600

    deleted extra directory

--- React-Course/src/api/guestSession.ts
+++
@@ -1,8 +0,0 @@
-import {NewInstance} from './base.api';
-const guestEP = 'authentication/guest_session/new';
-
-export const guestSession = async () => {
-	const response = await NewInstance.get(guestEP);
-    return response;
-};
-
